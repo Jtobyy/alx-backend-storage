8d72e3d79462845459d348779e67a6821985b4f6
Jtobyy <jtobi8161@gmail.com>
2022-04-26 01:39:31 +0100

    Finally ISNULL to the rescue

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 --  Lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (split - formed) AS
-lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC
+SELECT band_name, IFNULL(split - formed, 2020 - formed) AS lifespan
+FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
