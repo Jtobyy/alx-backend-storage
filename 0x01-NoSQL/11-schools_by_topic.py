3bffb3a367d3c8d20e1e59cf76700a6766e4ad0f
Jtobyy <jtobi8161@gmail.com>
2022-04-30 03:50:03 +0100

    11. Where can I learn Python?

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+Where can I learn Python?
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''returns the list of school having a specific topic'''
+    return list(mongo_collection.find({'topics': 'Python'}))
